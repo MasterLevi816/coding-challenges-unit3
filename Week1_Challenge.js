80cf41b26c1efac310b74d37c261fae08f090550
MasterLevi816 <oliviaransin@gmail.com>
2023-06-29 18:58:25 -0500

    area of triangle

---
+++ Week1_Challenge.js
@@ -0,0 +1,18 @@
+const input = require('readline-sync');
+
+function triangle () {
+
+let base = input.question ('Enter the number for triangle base: ');
+
+let height = input.question ('Enter the number for triangle height: ');
+
+let area = (base * height) / 2;
+
+  console.log(`The area of the triangle is ${area}`);
+  
+  return area;
+
+};
+
+triangle()
+
