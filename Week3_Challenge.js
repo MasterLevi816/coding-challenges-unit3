e654d64b9c564724ee9d2981885979c92f0d4699
MasterLevi816 <oliviaransin@gmail.com>
2023-07-13 19:19:10 -0500

    longest word

--- Week3_Challenge.js
+++ Week3_Challenge.js
@@ -1,15 +1,22 @@
-/*Have the function LongestWord(sen) take the sen parameter being passed and return the longest word in the string. 
-If there are two or more words that are the same length, return the first word from the string with that length. 
+/*Have the function LongestWord(sen) take the sen parameter being passed and return the longest word in the string.
+If there are two or more words that are the same length, return the first word from the string with that length.
 Ignore punctuation and assume sen will not be empty. Words may also contain numbers, for example "Hello world123 567"
 */
 
+function LongestWord(sen) {
 
-function LongestWord(sen) { 
+   const splitWord = sen.split(" ");
+    let longestWord = '';
 
-    // code goes here  
-    return sen; 
-  
-  }
-     
-  // keep this function call here 
-  console.log(LongestWord(readline()));
+    for (let i = 0; i < splitWord.length; i++){
+      if (splitWord[i].length > longestWord.length)
+        longestWord = splitWord[i]
+    }
+
+    return longestWord;
+
+
+}
+
+  console.log(LongestWord("Test which word is longestesttest"));
+  console.log(LongestWord("Second test for long word"));
