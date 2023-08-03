76ee3852e72af610526039bffa9411d2fb19415d
MasterLevi816 <oliviaransin@gmail.com>
2023-08-03 14:21:55 -0500

    character instance count in str

--- Week6_Challenge.js
+++ Week6_Challenge.js
@@ -2,3 +2,16 @@
 returns the number of times the first string (the single character) is found in the second string.
 */
 
+function charCount(myChar, str) {
+	let count = 0;
+	for (let i = 0; i < str.length; i++) {
+		if (str[i] === myChar) {
+			count++;
+		}
+	}
+	return count;
+}
+
+let result = charCount("a", "approach");
+console.log(result);
+
